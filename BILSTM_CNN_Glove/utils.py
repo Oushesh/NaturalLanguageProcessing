269da65634cac8b9d1d64adb261b57bdf2b60bdf
Oushesh <oushesh@gmail.com>
2018-08-07 18:27:40 +0200

    Updated with Full working Glove
    BILSTM_NER_GLOVE

---
+++ BILSTM_CNN_Glove/utils.py
@@ -0,0 +1,227 @@
+
+#Adapted from Kamalkraj NER BILSTM
+#https://github.com/kamalkraj/Named-Entity-Recognition-with-Bidirectional-LSTM-CNNs/blob/master/prepro.py
+import numpy as np
+import random
+from keras.preprocessing.sequence import pad_sequences
+
+def readfile(filename):
+    '''
+    read file
+    return format :
+    [ ['EU', 'B-ORG'], ['rejects', 'O'], ['German', 'B-MISC'], ['call', 'O'], ['to', 'O'], ['boycott', 'O'], ['British', 'B-MISC'], ['lamb', 'O'], ['.', 'O'] ]
+    '''
+    f = open(filename)
+    sentences = []
+    sentence = []
+    for line in f:
+        if len(line)==0 or line.startswith('-DOCSTART') or line[0]=="\n":
+            if len(sentence) > 0:
+                sentences.append(sentence)
+                sentence = []
+            continue
+        splits = line.split(' ')
+        sentence.append([splits[0],splits[-1]])
+
+    if len(sentence) >0:
+        sentences.append(sentence)
+        sentence = []
+    return sentences
+
+def getCasing(word, caseLookup):   
+    casing = 'other'
+    
+    numDigits = 0
+    for char in word:
+        if char.isdigit():
+            numDigits += 1
+            
+    digitFraction = numDigits / float(len(word))
+    
+    if word.isdigit(): #Is a digit
+        casing = 'numeric'
+    elif digitFraction > 0.5:
+        casing = 'mainly_numeric'
+    elif word.islower(): #All lower case
+        casing = 'allLower'
+    elif word.isupper(): #All upper case
+        casing = 'allUpper'
+    elif word[0].isupper(): #is a title, initial char upper, then all lower
+        casing = 'initialUpper'
+    elif numDigits > 0:
+        casing = 'contains_digit'
+    
+   
+    return caseLookup[casing]
+    
+
+def createBatches(data):
+    l = []
+    for i in data:
+        l.append(len(i[0]))
+    l = set(l)
+    batches = []
+    batch_len = []
+    z = 0
+    for i in l:
+        for batch in data:
+            if len(batch[0]) == i:
+                batches.append(batch)
+                z += 1
+        batch_len.append(z)
+    return batches,batch_len
+
+def createBatches(data):
+    l = []
+    for i in data:
+        l.append(len(i[0]))
+    l = set(l)
+    batches = []
+    batch_len = []
+    z = 0
+    for i in l:
+        for batch in data:
+            if len(batch[0]) == i:
+                batches.append(batch)
+                z += 1
+        batch_len.append(z)
+    return batches,batch_len
+
+def createMatrices(sentences, word2Idx, label2Idx, case2Idx,char2Idx):
+    unknownIdx = word2Idx['UNKNOWN_TOKEN']
+    paddingIdx = word2Idx['PADDING_TOKEN']    
+        
+    dataset = []
+    
+    wordCount = 0
+    unknownWordCount = 0
+    
+    for sentence in sentences:
+        wordIndices = []    
+        caseIndices = []
+        charIndices = []
+        labelIndices = []
+        
+        for word,char,label in sentence:  
+            wordCount += 1
+            if word in word2Idx:
+                wordIdx = word2Idx[word]
+            elif word.lower() in word2Idx:
+                wordIdx = word2Idx[word.lower()]                 
+            else:
+                wordIdx = unknownIdx
+                unknownWordCount += 1
+            charIdx = []
+            for x in char:
+                charIdx.append(char2Idx[x])
+            #Get the label and map to int            
+            wordIndices.append(wordIdx)
+            caseIndices.append(getCasing(word, case2Idx))
+            charIndices.append(charIdx)
+            labelIndices.append(label2Idx[label])
+           
+        dataset.append([wordIndices, caseIndices, charIndices, labelIndices]) 
+        
+    return dataset
+
+def iterate_minibatches(dataset,batch_len): 
+    start = 0
+    for i in batch_len:
+        tokens = []
+        caseing = []
+        char = []
+        labels = []
+        data = dataset[start:i]
+        start = i
+        for dt in data:
+            t,c,ch,l = dt
+            l = np.expand_dims(l,-1)
+            tokens.append(t)
+            caseing.append(c)
+            char.append(ch)
+            labels.append(l)
+        yield np.asarray(labels),np.asarray(tokens),np.asarray(caseing),np.asarray(char)
+
+def addCharInformation(Sentences):
+    for i,sentence in enumerate(Sentences):
+        for j,data in enumerate(sentence):
+            chars = [c for c in data[0]]
+            Sentences[i][j] = [data[0],chars,data[1]]
+    return Sentences
+
+def padding(Sentences):
+    maxlen = 52
+    for sentence in Sentences:
+        char = sentence[2]
+        for x in char:
+            maxlen = max(maxlen,len(x))
+    for i,sentence in enumerate(Sentences):
+        Sentences[i][2] = pad_sequences(Sentences[i][2],52,padding='post')
+    return Sentences
+
+
+#Method to compute the accruarcy. Call predict_labels to get the labels for the dataset
+def compute_f1(predictions, correct, idx2Label): 
+    label_pred = []    
+    for sentence in predictions:
+        label_pred.append([idx2Label[element] for element in sentence])
+        
+    label_correct = []    
+    for sentence in correct:
+        label_correct.append([idx2Label[element] for element in sentence])
+            
+    
+    #print label_pred
+    #print label_correct
+    
+    prec = compute_precision(label_pred, label_correct)
+    rec = compute_precision(label_correct, label_pred)
+    
+    f1 = 0
+    if (rec+prec) > 0:
+        f1 = 2.0 * prec * rec / (prec + rec);
+        
+    return prec, rec, f1
+
+def compute_precision(guessed_sentences, correct_sentences):
+    assert(len(guessed_sentences) == len(correct_sentences))
+    correctCount = 0
+    count = 0
+    
+    
+    for sentenceIdx in range(len(guessed_sentences)):
+        guessed = guessed_sentences[sentenceIdx]
+        correct = correct_sentences[sentenceIdx]
+        assert(len(guessed) == len(correct))
+        idx = 0
+        while idx < len(guessed):
+            if guessed[idx][0] == 'B': #A new chunk starts
+                count += 1
+                
+                if guessed[idx] == correct[idx]:
+                    idx += 1
+                    correctlyFound = True
+                    
+                    while idx < len(guessed) and guessed[idx][0] == 'I': #Scan until it no longer starts with I
+                        if guessed[idx] != correct[idx]:
+                            correctlyFound = False
+                        
+                        idx += 1
+                    
+                    if idx < len(guessed):
+                        if correct[idx][0] == 'I': #The chunk in correct was longer
+                            correctlyFound = False
+                        
+                    
+                    if correctlyFound:
+                        correctCount += 1
+                else:
+                    idx += 1
+            else:  
+                idx += 1
+    
+    precision = 0
+    if count > 0:    
+        precision = float(correctCount) / count
+        
+    return precision
